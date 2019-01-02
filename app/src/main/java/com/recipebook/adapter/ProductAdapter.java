d32a928781fa5571111c09e4aaafbb22454dd3c2
craftbox2245 <35990086+craftbox2245@users.noreply.github.com>
2019-01-02 18:29:45 +0530

    update

--- app/src/main/java/com/recipebook/adapter/ProductAdapter.java
+++ app/src/main/java/com/recipebook/adapter/ProductAdapter.java
@@ -1,6 +1,10 @@
 package com.recipebook.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.kogitune.activitytransition.ActivityTransitionLauncher;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.recipebook.R;
+import com.recipebook.WallpaperBoardPreviewActivity;
 import com.recipebook.model.ProductModel;
 
 import java.util.ArrayList;
@@ -59,6 +65,28 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
                         .build();
                 imageLoader.displayImage(data.get(position).getImagePath(), holder.homeImg, options);
             }
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        Bitmap bitmap = null;
+                        if (holder.homeImg.getDrawable() != null) {
+                            bitmap = ((BitmapDrawable) holder.homeImg.getDrawable()).getBitmap();
+                        }
+                        final Intent intent = new Intent(context, WallpaperBoardPreviewActivity.class);
+                        intent.putExtra("url", data.get(position).getImagePath());
+                        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        ActivityTransitionLauncher.with((AppCompatActivity) context)
+                                .from(holder.homeImg, "image")
+                                .image(bitmap)
+                                .launch(intent);
+                    } catch (Exception e) {
+                        //WallpaperBoardApplication.sIsClickable = true;
+                    }
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
